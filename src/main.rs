e60067886e2bca82d7ade7342033e7a925f562a2
mikit <mikihito0906@gmail.com>
2019-08-16 15:52:03 +0900

    Add crash recovery app

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate mikrodb;
+
+use mikrodb::database::Database;
+use std::result::Result;
+
+fn main() {
+    let mut db: Database<i32, i32> = Database::new("main.log", "main.db").unwrap();
+    let mut tx = db.begin_transaction().unwrap();
+
+    println!("Start");
+    for k in 0..100000 {
+        match tx.read(k) {
+            Result::Err(_) => {
+                println!("Record ({}, NA)", k);
+                tx.create(k, -1).unwrap();
+            },
+            Result::Ok(v) => {
+                println!("Record ({}, {})", k, v);
+            }
+        }
+    }
+    tx.commit().unwrap();
+
+    for v in 0..100000 {
+        println!("v = {}",v);
+        let mut tx = db.begin_transaction().unwrap();
+        for k in 0..100000 {
+            tx.update(k, v).unwrap();
+        }
+        tx.commit().unwrap();
+    }
+}
